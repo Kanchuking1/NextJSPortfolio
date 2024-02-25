23284e4767cbcfbb327522f753b9277b2add991e
Wahib Kapdi <kanchuking1@gmail.com>
2024-02-25 19:37:57 +0530

    Added About us Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import Layout from './Layout'
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
-        <span>Rights</span>
-        <Link href="/" target="_blank">Wahib Kapdi</Link>
-        <Link href="mailto:wahibkapdi2212@gmail.com">Get In Touch</Link>
+    <footer className="w-full border-t-2 !bg-dark text-light border-solid border-dark font-medium text-lg">
+        <Layout className='py-8 flex items-center justify-between !bg-dark'>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+            <Link href="/" target="_blank">Wahib Kapdi</Link>
+            <Link href="mailto:wahibkapdi2212@gmail.com">Get In Touch</Link>
+        </Layout>
     </footer>
   )
 }
