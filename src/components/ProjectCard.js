fc37da5949d1d5d558d8aa11b0895947a961f142
Wahib Kapdi <kanchuking1@gmail.com>
2024-03-11 10:41:40 +0530

    Removed Checkout Button

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -18,12 +18,15 @@ import {motion} from "framer-motion"
 const ProjectCard = ({ id, title, intro, target, alt, thumbnail, topics, tech }) => {
 
     return <motion.div id={`${id}`} 
-        className="w-full h-[500px] flex flex-col border-2 border-dark items-center rounded-2xl relative bg-light"
-        whileHover={{scale: 1.05}}>
+        className="w-full h-[450px] flex flex-col border-2 border-dark items-center rounded-2xl relative bg-light"
+        whileHover={{scale: 1.05}} onClick={() => {
+            console.log("Here at " + target);
+            window.open(target, "_blank");
+        }}>
         <figure className='h-3/5 p-4 w-full flex items-center justify-center overflow-hidden rounded-2xl'>
             <img src={thumbnail} alt={alt} className='w-full h-full object-cover rounded-2xl border-[1px] border-dark'/>
         </figure>
-        <div className="flex flex-col bg-light px-4 justify-between w-full">
+        <div className="flex flex-col px-4 justify-between w-full">
             <div className='w-full'>
                 <div className='flex overflow-hidden whitespace-nowrap my-1'>
                     {topics.map((topic, index) => {
@@ -39,7 +42,11 @@ const ProjectCard = ({ id, title, intro, target, alt, thumbnail, topics, tech })
             <h1 className='capitalize font-bold text-xl'>{title}</h1>
             <p>{intro}</p>
         </div>
-        <div className='absolute bottom-5 right-5 bg-dark rounded-xl text-light px-4 py-2 font-bold'><Link href={target} target="_blank">Checkout</Link></div>
+        {/* <div className='absolute bottom-5 right-5 bg-dark rounded-xl text-light px-4 py-2 font-bold'>
+            <Link href={target} target="_blank">
+                Checkout
+            </Link>
+        </div> */}
     </motion.div>
 }
 
