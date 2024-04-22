fe598c6b3e54a913b38c5686dcd5562a0bcbaf44
Wahib Kapdi <kanchuking1@gmail.com>
2024-04-22 21:50:37 +0530

    About Me Page Fixed

--- src/pages/about.js
+++ src/pages/about.js
@@ -55,17 +55,17 @@ const About= () => {
                             Biography
                         </h2>
                         <p className='font-medium'>
-                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                            Hi, I'm Wahib, a software engineer with a passion for creating beautiful, functional, 
                             and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
                             new and innovative ways to bring my clients' visions to life.
                         </p>
                         <p className='my-4 font-medium'>
-                            I believe that design is about more than just making things look pretty – it's about solving problems and 
-                            creating intuitive, enjoyable experiences for users. 
+                            My experience lies in varied field of engineering including Machine Learning, Robotics,
+                             Game Development and Web development. 
                         </p>
                         <p className='font-medium'>
-                            Whether I'm working on a website, mobile app, or 
-                            other digital product, I bring my commitment to design excellence and user-centered thinking to 
+                            Whether I'm working on a website, mobile app, ML model or 
+                            game, I bring my commitment to design excellence and user-centered thinking to 
                             every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                         </p>
                     </div>
